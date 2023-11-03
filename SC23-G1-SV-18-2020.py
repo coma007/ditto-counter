4b12bf0b953b97e8098381750f6f3c3e99f38fd2
coma007 <milicasladakovic@gmail.com>
2023-11-03 02:26:39 +0100

    refactor: script cleanup

--- SC23-G1-SV-18-2020.py
+++ SC23-G1-SV-18-2020.py
@@ -17,52 +17,26 @@ if __name__ == "__main__":
 
     i = 0
     for path in images:
-        image = load_image(path)
-
-        hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-
-        lower_purple = np.array([130, 40, 50])  # Adjust these values to match the specific shade of purple you want to detect
-        upper_purple = np.array([180, 200, 220])
-
-        purple_mask = cv2.inRange(hsv_image, lower_purple, upper_purple) 
-        purple_extracted = cv2.bitwise_and(image, image, mask=purple_mask)
-        image = purple_extracted
-        ret, image_bin = cv2.threshold(purple_mask, 1, 255, cv2.THRESH_BINARY)
-        
-        kernel = np.ones((3,3), np.uint8) 
-
-        opening = cv2.morphologyEx(image_bin, cv2.MORPH_OPEN, kernel, iterations = 3) # otvaranje
-        closing = cv2.morphologyEx(image_bin, cv2.MORPH_CLOSE, kernel, iterations = 6) # zatvaranje
-        dilation = cv2.morphologyEx(image_bin, cv2.MORPH_DILATE, kernel, iterations = 6) # dilacija
-        erosion = cv2.morphologyEx(image_bin, cv2.MORPH_ERODE, kernel, iterations = 3) # erozija
-        
-        sure_bg = cv2.dilate(erosion, kernel, iterations=10)
-        dist_transform = cv2.distanceTransform(dilation, cv2.DIST_L2, maskSize=5) #  DIST_L2 - Euklidsko rastojanje
-
-        ret, sure_fg = cv2.threshold(dist_transform, 0.55 * dist_transform.max(), 255, 0) 
-        sure_fg = np.uint8(sure_fg)
-        unknown = cv2.subtract(sure_bg, sure_fg)
-
-        ret, markers = cv2.connectedComponents(sure_fg)
-        markers = markers+1
-        markers[unknown==255] = 0
-        
-        markers = cv2.watershed(image, markers)
-        image[markers == -1] = [255, 0, 0]
-        rgba_img = label2rgb(markers)
-        unique_colours = {x for l in markers for x in l}
-        number_of_dittos = len(unique_colours) - 2
 
         title = path.split("/")[-1]
-        print(f"{title}-{counts[title]}-{number_of_dittos}")
+        image = load_image(path)
 
-        actual_values[i] = counts[title]
-        predicted_values[i] = number_of_dittos
+        hsv_image = convert_to_hsv(image)
+        purple_mask = extract_purple(hsv_image)
+        _, image_bin = convert_to_bin(purple_mask)
 
+        kernel = np.ones((3,3), np.uint8) 
+
+        opening, closing, dilation, erosion = get_morphological_features(image_bin, kernel, 6)
+        dist_transform = distance_transform(dilation) 
+        sure_fg, sure_bg, unknown = extract_foreground(dist_transform, erosion, kernel, 3, 0.55)
+        number_of_dittos = watershed(image, sure_fg, unknown)
+
+        print_result(title, counts[title], number_of_dittos)
+        save_result(i, actual_values, predicted_values, counts[title], number_of_dittos)
         i += 1
 
-    absolute_differences = np.abs(predicted_values - actual_values)
-    mae = np.mean(absolute_differences)
+    mae = calculate_mae(predicted_values, actual_values)
     print(mae)
 
     
--- preprocessing.py
+++ preprocessing.py
@@ -4,7 +4,6 @@ from os.path import isfile, join
 import numpy as np
 import cv2
 from matplotlib import pyplot as plt
-from scipy import signal
 import csv
 
 def read_csv(path):
@@ -20,8 +19,6 @@ def read_csv(path):
             data_dict[key] = value
 
     return data_dict
-
-
 
 def parse_args():
     parser = argparse.ArgumentParser()
@@ -42,26 +39,58 @@ def display_image(image, color=False):
     else:
         plt.imshow(image, 'gray')
 
-def rgb_to_grayscale(image):
-    return cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+def convert_to_hsv(image):
+    return cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
+def extract_purple(hsv_image):
+    lower_purple = np.array([130, 40, 50]) 
+    upper_purple = np.array([180, 200, 220])
+    return cv2.inRange(hsv_image, lower_purple, upper_purple) 
+        
+def convert_to_bin(image):
+    return cv2.threshold(image, 1, 255, cv2.THRESH_BINARY)
+        
 
-def grayscale_to_bin(image):
-    _, image_bin = cv2.threshold(image, 0, 255, cv2.THRESH_OTSU-100)
-    return image_bin
+def get_morphological_features(image, kernel, iter):
+    opening = cv2.morphologyEx(image, cv2.MORPH_OPEN, kernel, iterations = iter)
+    closing = cv2.morphologyEx(image, cv2.MORPH_CLOSE, kernel, iterations = iter)
+    dilation = cv2.morphologyEx(image, cv2.MORPH_DILATE, kernel, iterations = iter)
+    erosion = cv2.morphologyEx(image, cv2.MORPH_ERODE, kernel, iterations = iter) 
+    return opening, closing, dilation, erosion
 
-def blur(image):
-    k_size = 5
-    k = (1./k_size*k_size) * np.ones((k_size, k_size))
-    image_blur = signal.convolve2d(image, k)
-    return image_blur
+def distance_transform(feature):
+    return cv2.distanceTransform(feature, cv2.DIST_L2, maskSize=5)        
 
-def invert(image):
-    return 255-image
+def extract_foreground(dist_transform, background_feature, kernel, iter, percentage):
 
+    sure_bg = cv2.dilate(background_feature, kernel, iterations=iter)
+    _, sure_fg = cv2.threshold(dist_transform, percentage * dist_transform.max(), 255, 0) 
+    sure_fg = np.uint8(sure_fg)
+    unknown = cv2.subtract(sure_bg, sure_fg)
+    return sure_fg, sure_bg, unknown
+
+def watershed(image, sure_fg, unknown):
+    _, markers = cv2.connectedComponents(sure_fg)
+    markers = markers+1
+    markers[unknown==255] = 0
+    markers = cv2.watershed(image, markers)
+    unique_colours = {x for l in markers for x in l}
+    return len(unique_colours) - 2
 
 def label2rgb(markers):
     cmap = plt.get_cmap('jet', len(np.unique(markers)))
     return cmap(markers)
+
+def print_result(title, actual_values, predicted_values):
+    print(f"{title}-{actual_values}-{predicted_values}")
+
+def save_result(index, actual_values_array, predicted_values_array, actual_values, predicted_values):
+    actual_values_array[index] = actual_values
+    predicted_values_array[index] = predicted_values
+
+def calculate_mae(predicted_values, actual_values):
+    absolute_differences = np.abs(predicted_values - actual_values)
+    return np.mean(absolute_differences)
+
 
 
